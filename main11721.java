12e61aba2fb0edb7b661b39872c37e74ae535f91
llshl <52540882+llshl@users.noreply.github.com>
2020-12-26 04:01:38 +0900

    Add files via upload

---
+++ main11721.java
@@ -0,0 +1,18 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main11721 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		for(int i=0;i<s.length();i++) {
+			System.out.print(s.charAt(i));
+			if(i%10==9) {
+				System.out.println("");
+			}
+		}
+	}
+}
