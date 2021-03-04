cbbbd9edbcc70b19e0e67b66e9e9692c6d420911
llshl <52540882+llshl@users.noreply.github.com>
2021-03-04 10:26:31 +0900

    baekjoon

---
+++ BFS_DFS/main11725bfs.java
@@ -0,0 +1,55 @@
+package BFS_DFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class main11725bfs {
+
+	public static int[] answer;
+	public static ArrayList<ArrayList<Integer>> graph;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		boolean[] visit = new boolean[N+1];
+		answer = new int[N+1];
+		
+		graph = new ArrayList<ArrayList<Integer>>();
+		for(int i=0;i<N+1;i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+		
+		visit[0] = true;
+		for(int i=0;i<N-1;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph.get(a).add(b);
+			graph.get(b).add(a);
+		}
+		bfs(visit,1);
+		for(int i=2;i<answer.length;i++) {
+			System.out.println(answer[i]);			
+		}
+	}
+
+	private static void bfs(boolean[] visit, int x) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(x);
+		visit[x] = true;
+		while(!q.isEmpty()) {
+			int temp = q.poll();
+			for(int i=0;i<graph.get(temp).size();i++) {
+				if(visit[graph.get(temp).get(i)] == false) {
+					visit[graph.get(temp).get(i)] = true;
+					q.add(graph.get(temp).get(i));
+					answer[graph.get(temp).get(i)] = temp;
+				}
+			}
+		}
+	}
+}
---
+++ BFS_DFS/main1967dfs.java
@@ -0,0 +1,72 @@
+package BFS_DFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class Poi{
+	int num;
+	int weight;
+	public Poi(int num, int weight) {
+		super();
+		this.num = num;
+		this.weight = weight;
+	}
+}
+
+public class main1967dfs {
+
+	public static int N;
+	public static int maxWeight = 0;
+	public static int maxNode = 0;
+	public static ArrayList<Poi>[] graph;
+	public static boolean[] visit;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		graph = new ArrayList[N+1];
+		visit = new boolean[N+1];
+		for(int i=0;i<=N;i++) {
+			graph[i] = new ArrayList<Poi>();
+		}
+		for(int i=0;i<N-1;i++) {	//N-1번반복
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			graph[a].add(new Poi(b,c)); 	//a -> b로 c만큼 가중치
+			graph[b].add(new Poi(a,c));
+		}
+		
+		for(int i=0;i<graph[1].size();i++) {
+			visit[1] = true;
+			dfs(graph[1].get(i).num, graph[1].get(i).weight);
+			visit[1] = false;
+		}
+		maxWeight = 0;
+		for(int i=0;i<graph[maxNode].size();i++) {
+			visit[maxNode] = true;
+			dfs(graph[maxNode].get(i).num,graph[maxNode].get(i).weight);
+			visit[maxNode] = false;
+		}
+		System.out.println(maxWeight);
+	}
+	
+	private static void dfs(int n, int weight) { 
+		visit[n] = true;
+		for(int i=0;i<graph[n].size();i++) {
+			if(visit[graph[n].get(i).num] == false) {
+				visit[graph[n].get(i).num] = true;
+				dfs(graph[n].get(i).num,weight+graph[n].get(i).weight);
+				visit[graph[n].get(i).num] = false;
+			}
+		}
+		if(maxWeight < weight) {
+			maxWeight = weight;
+			maxNode = n;
+		}
+		visit[n] = false;
+	}
+}
