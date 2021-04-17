62b7be323bb5bfb9e194a8ae9bfe34f5471d12b7
llshl <52540882+llshl@users.noreply.github.com>
2021-04-18 08:15:06 +0900

    baekjoon

--- DP/main11052.java
+++ DP/main11052.java
@@ -3,28 +3,23 @@ package DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-public class main11053ReRe {
+public class main11052 {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		int answer = -1;
+		int[] arr = new int[N+1];
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		for(int i=0;i<N;i++) {
+		for(int i=1;i<=N;i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		int[] dp = new int[N];
-		for(int i=0;i<N;i++) {
-			dp[i] = 1;
-			for(int j=0;j<i;j++) {
-				if(arr[j] < arr[i]) {
-					dp[i] = Math.max(dp[j]+1, dp[i]); 
-				}
+		int answer = -1;
+		int dp[] = new int[N+1];
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=i;j++) {
+				dp[i] = Math.max(dp[i],dp[i-j]+arr[j]);
 			}
 			answer = Math.max(answer, dp[i]);
 		}
--- DP/main2352.java
+++ DP/main2352.java
@@ -3,11 +3,9 @@ package DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-public class main11053ReRe {
+public class main2352 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,15 +17,7 @@ public class main11053ReRe {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		int[] dp = new int[N];
-		for(int i=0;i<N;i++) {
-			dp[i] = 1;
-			for(int j=0;j<i;j++) {
-				if(arr[j] < arr[i]) {
-					dp[i] = Math.max(dp[j]+1, dp[i]); 
-				}
-			}
-			answer = Math.max(answer, dp[i]);
-		}
-		System.out.println(answer);
+
 	}
+
 }
